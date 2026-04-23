71c7fd807769182dc955f1b256e997908d4d5357
MohamedAhmedMohamedAmmar <mohamed.ammar2872006@gmail.com>
2026-04-23 21:10:50 +0200

    init

---
+++ src/main/java/com/example/sport_finder/SportFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.sport_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sport_finder/SportFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sport_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
